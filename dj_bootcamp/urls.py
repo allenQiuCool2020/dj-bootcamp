2cd4b2b5cb1c2a6a7031d328edcb140ae13d8d61
Allen Qiu <118089569+allenQiuCool2020@users.noreply.github.com>
2023-12-14 11:41:21 +0800

    Commit on Dec 14
    Commit on Dec 14

--- dj_bootcamp/urls.py
+++ dj_bootcamp/urls.py
@@ -20,10 +20,12 @@ from products.views import (
     home_view,
     product_detail,
     product_api_detail,
+    product_list_view,
 )
 
 urlpatterns = [
     path('search/',home_view),
+    path('products',product_list_view),
     path('products/<int:pk>/',product_detail),
     path('api/products/<int:pk>/',product_api_detail),
     path('admin/', admin.site.urls),
--- products/views.py
+++ products/views.py
@@ -4,15 +4,24 @@ from .models import Product
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    print(args, kwargs)
-    return HttpResponse("<h1>Helloworld<h1/>")
+    # print(args, kwargs)
+    # return HttpResponse("<h1>Helloworld<h1/>")
+    context = {
+        "name": "Allen"
+    }
+    return render(request, "home.html", context)
 
 def product_detail(request, pk):
     try:
         obj = Product.objects.get(id=pk)
     except Product.DoesNotExist:
         raise Http404
-    return HttpResponse(f"Product id {obj.id}")
+    context = {
+        "object": obj
+    }
+    print(request.path)
+    # return HttpResponse(f"Product id {obj.id}")
+    return render(request, "products/detail.html", context)
 
 def product_api_detail(request, pk):
     try:
@@ -20,3 +29,10 @@ def product_api_detail(request, pk):
     except Product.DoesNotExist:
         return JsonResponse({"message": "not found"})
     return JsonResponse({"id": obj.id})
+
+def product_list_view(request, *args, **kwargs):
+    qs = Product.objects.all()
+    context = {
+        "object_list": qs
+    }
+    return render(request, "products/list.html", context)
